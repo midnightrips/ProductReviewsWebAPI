648cc0eecf40b64c64406a691ba27cc5e3c2ed4e
midnightrips <sg3584@alum.barnard.edu>
2024-03-06 12:13:33 -0500

    Completing products controller

--- ProjectReviewsWebAPI/Controllers/ProductsController.cs
+++ ProjectReviewsWebAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using ProjectReviewsWebAPI.Data;
+using ProjectReviewsWebAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,34 +20,62 @@ namespace ProductReviewsWebAPI.Controllers
 
         // GET: api/<ProductsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var product = _context.Products.ToList();
+            return Ok(product);
         }
 
         // GET api/<ProductsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
         }
 
         // POST api/<ProductsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Product product)
         {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return StatusCode(201, product);
         }
 
         // PUT api/<ProductsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Product product)
         {
+            var existingProduct = _context.Products.Where(p => p.Id == id).SingleOrDefault();
+
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+
+            product = existingProduct;
+
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return Ok(product);
         }
 
         // DELETE api/<ProductsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var product = _context.Products.Where(p => p.Id == id).SingleOrDefault();
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
